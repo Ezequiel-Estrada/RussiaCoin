203a5c9cc79e114b02404be0631e226494142985
RussiaCoin RC V1.1 <RussiaCoinDotInfo@users.noreply.github.com>
2017-06-24 17:53:47 +1000

    Ver 1.2.0.1
    change "expensive" to "wealth"

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Россия");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-дорогая"
+#define CLIENT_VERSION_SUFFIX   "-богатство"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
